7907fcd60b2dc99ff89e5aec4b23fb6c1b17e421
jnsougata <jnsougata@gmail.com>
2025-04-08 16:48:04 +0530

    Remove unused __all__ declaration from __init__.py

--- fractal/__init__.py
+++ fractal/__init__.py
@@ -20,16 +20,3 @@ from .converter import *
 from .kvclient import KVStore
 from .query import *
 from .schema import *
-
-__all__ = [
-    "__title__",
-    "__license__",
-    "__copyright__",
-    "__author__",
-    "__version__",
-    "DB",
-    "KVStore",
-    "Schema",
-    "Field",
-    "cond",
-]
